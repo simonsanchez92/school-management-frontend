dd54bb28f20af2ce187a33bcf6ac108bd9207e78
Simon Sanchez <simonsanchez.1992@gmail.com>
2022-12-22 19:10:07 -0300

    working on subject add

--- src/components/subject/add/AddSubject.tsx
+++ src/components/subject/add/AddSubject.tsx
@@ -1,4 +1,31 @@
-const AddSubject = () => {
+import { useState, ChangeEvent } from "react";
+
+type subject = {
+  id: string;
+  name: string;
+  schoolYear: number;
+};
+
+const AddSubject = (): JSX.Element => {
+  const [id, setId] = useState<string>("");
+  const [subjectName, setSubjectName] = useState<string>("");
+  const [schoolYear, setSchoolYear] = useState(1);
+
+  const handleId = (e: ChangeEvent<HTMLInputElement>) => {
+    setId(e.target.value);
+  };
+
+  const handleSubjectName = (e: ChangeEvent<HTMLInputElement>) => {
+    setSubjectName(e.target.value);
+  };
+  const handleSchoolYear = (e: ChangeEvent<HTMLInputElement>) => {
+    setSchoolYear(+e.target.value);
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    console.log({ id, subjectName, schoolYear });
+  };
   return (
     <div>
       <div className="page-header">
@@ -17,7 +44,7 @@ const AddSubject = () => {
           <div className="col-sm-12">
             <div className="card">
               <div className="card-body">
-                <form className="w-100">
+                <form className="w-100" onSubmit={(e) => handleSubmit(e)}>
                   <div className="row">
                     <div className="col-12 ">
                       <h5 className="form-title subject-info d-flex justify-content-between mb-4">
@@ -33,6 +60,8 @@ const AddSubject = () => {
                           type="text"
                           className="form-control"
                           placeholder="Subject ID"
+                          value={id}
+                          onChange={(e) => handleId(e)}
                         />
                       </div>
                     </div>
@@ -45,24 +74,30 @@ const AddSubject = () => {
                           type="text"
                           className="form-control"
                           placeholder="Subject Name"
+                          value={subjectName}
+                          onChange={(e) => handleSubjectName(e)}
                         />
                       </div>
                     </div>
                     <div className="col-12 col-sm-4 my-2">
                       <div className="form-group d-flex flex-column align-items-start">
                         <label htmlFor="" className="form-label">
-                          Class
+                          School Year
                         </label>
                         <input
-                          type="text"
+                          type="number"
                           className="form-control"
-                          placeholder="class"
+                          placeholder="School Year"
+                          min="1"
+                          max="5"
+                          value={schoolYear}
+                          onChange={(e) => handleSchoolYear(e)}
                         />
                       </div>
                     </div>
                     <div className="col-12 my-5">
                       <div className="student-submit d-flex justify-start">
-                        <button className="btn btn-primary">Register</button>
+                        <button className="btn btn-primary px-5">Submit</button>
                       </div>
                     </div>
                   </div>
